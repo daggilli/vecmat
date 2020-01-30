b8584a52e76c34fee407d0462a4841fc975b7b20
daggilli <daggillies@gmail.com>
2020-01-29 18:24:23 -0600

    feat: use pointer-based storage instead of vectors [develop]

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <complex>
+#include <iomanip>
 #include <iostream>
 #include <vector>
 
@@ -47,33 +48,90 @@ int main() {
   std::cout << b << "\n\n";
   std::cout << c << "\n"; */
 
+  /*
+    VecMat::Vector<double> v = {1, 2, 3, 4};
+    VecMat::Vector<double> w = {1, 2, 3};
+    VecMat::Matrix<double> n = {{7, 8, 9, 10}, {11, 12, 13, 14}, {15, 16, 17, 18}};
+    VecMat::Matrix<double> s = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    VecMat::Vector<double> r = n * v;
+
+    std::cout << r << "\n";
+
+    r = w * n;
+
+    std::cout << r << "\n";
+
+    std::cout << w * w << '\n';
+
+    std::cout << s * n << '\n';
+
+    std::cout << VecMat::transpose(n) << '\n';
+
+    auto x = s * n;
+
+    std::cout << x << '\n';
+
+    auto y = std::move(x);
+
+    std::cout << y << '\n';
+    std::cout << "mv\n";
+    std::cout << x << '\n';
+
+    VecMat::Vector<std::complex<double>> cv = {1.0 + 0.5i, 1.0i, 3.0 - 2.0i};
+
+    std::cout << cv << '\n';
+
+    std::cout << cv.total() << '\n';
+
+    std::cout << w.norm() << '\n';
+
+    std::cout << cv.norm() << '\n';
+
+    std::cout << w * 3.3 << '\n';
+    std::cout << 6.2 * w << '\n';
+
+    v = {1, 2, 3};
+    w = {4, 5, 6};
+
+    std::cout << VecMat::angle(v, w) << '\n';
+
+    cv = {1.0 + 1.0i, 2.0 - 1.0i};
+    VecMat::Vector<std::complex<double>> cw = {2.0 - 3.0i, 4.0 + 1.0i};
+
+    std::cout << cv.norm() << " " << cw.norm() << '\n';
+
+    std::cout << VecMat::angle(cv, cw) << '\n';
+
+    VecMat::Matrix<std::complex<double>> cm = {{1.0 + 1.0i, 2.0 - 1.0i}, {2.0 - 3.0i, 4.0 + 1.0i}};
+
+    std::cout << cm.trace() << '\n';
+
+    */
+
+#if 0
   VecMat::Vector<double> v = {1, 2, 3, 4};
   VecMat::Vector<double> w = {1, 2, 3};
   VecMat::Matrix<double> n = {{7, 8, 9, 10}, {11, 12, 13, 14}, {15, 16, 17, 18}};
   VecMat::Matrix<double> s = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-  VecMat::Vector<double> r = n * v;
+  // VecMat::Vector<double> r = n * v;
+  auto r = w * n;
 
-  std::cout << r << "\n";
+  std::cout << r << "\n---\n";
+  std::cout << s[1][2] << '\n';
 
-  r = w * n;
+  // s[1][1] = 17;
 
-  std::cout << r << "\n";
+  // std::cout << s[1][1] << '\n';
 
-  std::cout << w * w << '\n';
+  std::cout << s << '\n';
 
   std::cout << s * n << '\n';
 
-  std::cout << VecMat::transpose(n) << '\n';
+  auto sxn = s * n;
+  std::cout << sxn.trace() << '\n';
+  std::cout << w * w << '\n';
 
-  auto x = s * n;
-
-  std::cout << x << '\n';
-
-  auto y = std::move(x);
-
-  std::cout << y << '\n';
-  std::cout << "mv\n";
-  std::cout << x << '\n';
+  std::cout << n * v << '\n';
 
   VecMat::Vector<std::complex<double>> cv = {1.0 + 0.5i, 1.0i, 3.0 - 2.0i};
 
@@ -81,17 +139,19 @@ int main() {
 
   std::cout << cv.total() << '\n';
 
+  w = v;
+
+  std::cout << w << '\n';
+
+  VecMat::Vector<double> vv(std::move(w));
+
+  std::cout << vv << '\n';
+
+  w = std::move(vv);
+
+  std::cout << w << '\n';
+
   std::cout << w.norm() << '\n';
-
-  std::cout << cv.norm() << '\n';
-
-  std::cout << w * 3.3 << '\n';
-  std::cout << 6.2 * w << '\n';
-
-  v = {1, 2, 3};
-  w = {4, 5, 6};
-
-  std::cout << VecMat::angle(v, w) << '\n';
 
   cv = {1.0 + 1.0i, 2.0 - 1.0i};
   VecMat::Vector<std::complex<double>> cw = {2.0 - 3.0i, 4.0 + 1.0i};
@@ -100,8 +160,42 @@ int main() {
 
   std::cout << VecMat::angle(cv, cw) << '\n';
 
-  VecMat::Matrix<std::complex<double>> cm = {{1.0 + 1.0i, 2.0 - 1.0i}, {2.0 - 3.0i, 4.0 + 1.0i}};
+  std::cout << std::boolalpha << s.isSquare() << '\n';
+  std::cout << std::boolalpha << sxn.isSquare() << '\n';
 
-  std::cout << cm.trace() << '\n';
+  s[1][1] = 17;
+
+  VecMat::Matrix<double> g = {{3, 4, 22}, {14, 12, 30}, {26, 24, 42}};
+  std::cout << g.determinant() << '\n';
+
+  VecMat::Matrix<double> f = {{3, 4, 22, 4, 54, 4, 153},          {14, 12, 30, 12, 62, 18, 165},
+                              {26, 24, 42, 24, 111, 36, 183},     {42, 40, 58, 60, 135, 60, 207},
+                              {62, 60, 117, 90, 165, 90, 237},    {86, 126, 153, 126, 201, 126, 273},
+                              {171, 168, 195, 168, 243, 168, 315}};
+
+  std::cout << f.determinant() << '\n';
+
+  VecMat::Matrix<int64_t> h = {
+      {3, 4, 22, 4, 54}, {14, 12, 30, 12, 62}, {26, 24, 42, 24, 111}, {42, 40, 58, 60, 135}, {62, 60, 117, 90, 165}};
+
+  std::cout << h.determinant() << '\n';
+
+  VecMat::Matrix<int64_t> j = {{3, 4, 22, 4}, {14, 12, 30, 12}, {26, 24, 42, 24}, {42, 40, 58, 60}};
+
+  std::cout << j.determinant() << '\n';
+
+#endif
+
+  VecMat::Matrix<double> k = {{-0.700624, 0.340395, -0.846329, -0.663135, -0.503968, 0.823883, 0.978628, -0.0576061},
+                              {0.743513, 0.795462, -0.199831, 0.146875, 0.142725, -0.366635, 0.19976, 0.616893},
+                              {0.713103, 0.489651, 0.836863, 0.755844, -0.421905, -0.785994, 0.894424, -0.0806125},
+                              {-0.390112, -0.767406, 0.0110868, 0.244983, 0.24305, 0.309218, -0.36558, -0.794871},
+                              {-0.0184016, 0.263631, 0.390246, 0.312225, -0.578232, -0.892475, 0.0635684, -0.346404},
+                              {-0.383056, 0.11086, 0.357179, -0.872111, 0.794477, -0.334883, 0.701342, -0.953884},
+                              {0.267538, 0.600714, -0.0252267, 0.589941, -0.223907, 0.786855, 0.329574, -0.863015},
+                              {0.228132, -0.218749, 0.945731, -0.262868, 0.985404, 0.425773, -0.585217, 0.294802}};
+
+  std::cout << k.determinant() << '\n';
+
   return 0;
 }
--- vecmat.h
+++ vecmat.h
@@ -1,7 +1,9 @@
 #ifndef __VECMAT_H__
 #define __VECMAT_H__
+#include <algorithm>
 #include <cmath>
 #include <complex>
+#include <cstdint>
 #include <exception>
 #include <initializer_list>
 #include <iostream>
@@ -10,35 +12,64 @@
 #include <vector>
 
 namespace VecMat {
+  namespace {
+    constexpr std::size_t GAUSS_ELIM_DET_THRESHOLD = 7;
+  }
+
   template <typename T>
   class Matrix;
 
   template <typename T>
   class Vector {
    public:
-    using vectorType = typename std::vector<T>;
-    using iterator = typename vectorType::iterator;
-    using const_iterator = typename vectorType::const_iterator;
-    using reverse_iterator = typename vectorType::reverse_iterator;
-    using const_reverse_iterator = typename vectorType::const_reverse_iterator;
+    using iterator = T*;
+    using const_iterator = T const*;
+    using reverse_iterator = T*;
+    using const_reverse_iterator = T const*;
 
-    Vector() : zero(0.0) {}
-    Vector(const std::size_t n) : vec(n), zero(0.0) {}
-    Vector(const std::initializer_list<T> vlist) : vec(vlist), zero(0.0) {}
+    Vector() : vec(nullptr), sz(0), zero(0.0) {}
+    Vector(const std::size_t n) : vec(new T[n]), sz(n), zero(0.0) {}
+    Vector(const std::initializer_list<T> vlist) : vec(new T[vlist.size()]), sz(vlist.size()), zero(0.0) {
+      std::size_t ix = 0;
 
-    void reserve(const std::size_t n) { vec.reserve(n); }
-    void resize(const std::size_t n) { vec.resize(n); }
-    std::size_t size() const { return vec.size(); }
-    const T total() const { return std::accumulate(vec.cbegin(), vec.cend(), zero); }
+      for (auto& el : vlist) {
+        vec[ix++] = el;
+      }
+    }
+    Vector(const Vector<T>& v) : vec(new T[v.sz]), sz(v.sz), zero(v.zero) { std::copy(v.vec, v.vec + sz, vec); }
+    Vector(Vector<T>&& v) : vec(std::exchange(v.vec, nullptr)), sz(std::exchange(v.sz, 0)), zero(v.zero) {}
+    ~Vector() { delete[] vec; }
+    std::size_t size() const { return sz; }
+    const T total() const { return std::accumulate(vec, vec + sz, zero); }
     const Vector<T> conj() const {
-      Vector<T> v = this;
+      Vector<T> v(this);
       return v;
     }
     const double norm() const {
-      return std::sqrt(std::accumulate(vec.cbegin(), vec.cend(), 0.0, [](const T& s, const T& v) { return s + v * v; }));
+      return std::sqrt(std::accumulate(vec, vec + sz, 0.0, [](const T& s, const T& v) { return s + v * v; }));
+    }
+    Vector<T>& operator=(const Vector<T>& v) {
+      if (&v != this) {
+        if (sz != v.sz) {
+          delete[] vec;
+          vec = new T[v.sz];
+          sz = v.sz;
+        }
+        std::copy(v.vec, v.vec + sz, vec);
+      }
+      return *this;
+    }
+    Vector<T>& operator=(Vector<T>&& v) {
+      if (&v != this) {
+        vec = std::exchange(v.vec, nullptr);
+        sz = std::exchange(v.sz, 0);
+        zero = v.zero;
+      }
+      return *this;
     }
     T& operator[](const std::size_t i) { return vec[i]; }
     const T& operator[](const std::size_t i) const { return vec[i]; }
+
     friend Vector<T> operator+(const Vector<T>& lhs, const Vector<T>& rhs) {
       checkDimensions(lhs, rhs);
       Vector<T> v(lhs.size());
@@ -51,7 +82,7 @@ namespace VecMat {
       checkDimensions(lhs, rhs);
       Vector<T> v(lhs.size());
 
-      for (auto i = 0; i < lhs.size(); i++) v[i] = lhs[i] + rhs[i];
+      for (auto i = 0; i < lhs.size(); i++) v[i] = lhs[i] - rhs[i];
 
       return v;
     }
@@ -62,12 +93,13 @@ namespace VecMat {
     }
     Vector<T>& operator-=(const Vector<T>& rhs) {
       checkDimensions(*this, rhs);
-      for (auto i = 0; i < vec.size(); i++) vec[i] += rhs.vec[i];
+      for (auto i = 0; i < vec.size(); i++) vec[i] -= rhs.vec[i];
       return *this;
     }
+
     friend T operator*(const Vector<T>& lhs, const Vector<T>& rhs) {
       checkDimensions(lhs, rhs);
-      T dot;
+      T dot = 0;
       for (auto i = 0; i < lhs.size(); i++) {
         dot += lhs[i] * rhs[i];
       }
@@ -103,22 +135,23 @@ namespace VecMat {
     friend std::ostream& operator<<(std::ostream& os, const Vector<T>& v) {
       os << "|";
       std::string sep = "";
-      for (auto& e : v.vec) {
-        os << sep << e;
+      const_iterator it = v.cbegin();
+      while (it != v.cend()) {
+        os << sep << *it++;
         sep = " ";
       }
       os << "|";
       return os;
     }
 
-    inline iterator begin() noexcept { return vec.begin(); }
-    inline const_iterator cbegin() const noexcept { return vec.cbegin(); }
-    inline iterator end() noexcept { return vec.end(); }
-    inline const_iterator cend() const noexcept { return vec.cend(); }
-    inline reverse_iterator rbegin() noexcept { return vec.rbegin(); }
-    inline reverse_iterator crbegin() const noexcept { return vec.crbegin(); }
-    inline reverse_iterator rend() noexcept { return vec.rend(); }
-    inline reverse_iterator crend() const noexcept { return vec.crend(); }
+    inline iterator begin() noexcept { return vec; }
+    inline const_iterator cbegin() const noexcept { return vec; }
+    inline iterator end() noexcept { return vec + sz; }
+    inline const_iterator cend() const noexcept { return vec + sz; }
+    inline reverse_iterator rbegin() noexcept { return vec + sz; }
+    inline reverse_iterator crbegin() const noexcept { return vec + sz; }
+    inline reverse_iterator rend() noexcept { return vec; }
+    inline reverse_iterator crend() const noexcept { return vec; }
 
    private:
     static void checkDimensions(const Vector<T>& left, const Vector<T>& right) {
@@ -131,14 +164,15 @@ namespace VecMat {
         throw(std::runtime_error("Vector x matrix length mismatch"));
       }
     }
-    std::vector<T> vec;
+    T* vec;
+    std::size_t sz;
     T zero;
   };
 
   template <>
   const inline Vector<std::complex<double>> Vector<std::complex<double>>::conj() const {
-    Vector<std::complex<double>> v(vec.size());
-    for (auto i = 0; i < vec.size(); i++) {
+    Vector<std::complex<double>> v(sz);
+    for (auto i = 0; i < sz; i++) {
       v[i] = std::conj(vec[i]);
     }
     return v;
@@ -146,14 +180,13 @@ namespace VecMat {
 
   template <>
   const inline double Vector<std::complex<double>>::norm() const {
-    return std::sqrt(std::accumulate(vec.cbegin(), vec.cend(), 0.0, [](const double s, const std::complex<double> t) {
-      return s + std::abs(t * std::conj(t));
-    }));
+    return std::sqrt(std::accumulate(
+        vec, vec + sz, 0.0, [](const double s, const std::complex<double> t) { return s + std::abs(t * std::conj(t)); }));
   }
 
   template <typename T>
-  const inline double angle(const Vector<T>& u, const Vector<T>& v) {
-    return std::acos((u * v) / (u.norm() * v.norm()));
+  const inline T angle(const Vector<T>& u, const Vector<T>& v) {
+    return T(std::acos((u * v) / (u.norm() * v.norm())));
   }
 
   const inline std::complex<double> angle(const Vector<std::complex<double>>& u, const Vector<std::complex<double>>& v) {
@@ -162,39 +195,62 @@ namespace VecMat {
 
   template <typename T>
   class Matrix {
-    using matrixType = typename std::vector<Vector<T>>;
-    using iterator = typename matrixType::iterator;
-    using const_iterator = typename matrixType::const_iterator;
-    using reverse_iterator = typename matrixType::reverse_iterator;
-    using const_reverse_iterator = typename matrixType::const_reverse_iterator;
+    using iterator = T**;
+    using const_iterator = const T**;
+    using reverse_iterator = T**;
+    using const_reverse_iterator = const T**;
 
    public:
-    Matrix(const std::size_t n) : m(n), n(n), mat(n) {
-      for (auto i = 0; i < n; i++) mat[i].resize(n);
+    Matrix(const std::size_t n) : m(n), n(n), data(new T[n * n]), mat(new T*[n]) {
+      for (auto i = 0; i < n; i++) mat[i] = data + i * n;
     }
-    Matrix(const std::size_t m, const std::size_t n) : m(m), n(n), mat(m) {
+    Matrix(const std::size_t m, const std::size_t n) : m(m), n(n), data(new T[m * n]), mat(new T*[m]) {
       std::cout << "rc ctor " << m << " x " << n << "\n";
-      mat.resize(m);
-      for (auto i = 0; i < m; i++) mat[i].resize(n);
+      for (auto i = 0; i < m; i++) mat[i] = data + i * n;
     }
-    Matrix(const std::initializer_list<std::initializer_list<T>> mlist) : m(mlist.size()), n(0), mat(mlist.size()) {
-      auto mbeg = mat.begin();
+    Matrix(const std::pair<std::size_t, std::size_t>& sz)
+        : m(sz.first), n(sz.second), data(new T[sz.first * sz.first]), mat(new T*[sz.first]) {
+      std::cout << "rc sz ctor " << m << " x " << n << "\n";
+      for (auto i = 0; i < m; i++) mat[i] = data + i * n;
+    }
+    Matrix(const std::initializer_list<std::initializer_list<T>> mlist)
+        : m(mlist.size()), n(0), data(nullptr), mat(new T*[mlist.size()]) {
+      std::size_t ix = 0;
+      std::size_t jx;
       n = mlist.begin()->size();
+      data = new T[m * n];
       std::cout << "il ctor " << m << " x " << n << "\n";
-      for (auto& v : mlist) {
-        *mbeg++ = v;
+      for (auto& r : mlist) {
+        jx = 0;
+        for (auto& el : r) {
+          data[(ix * n) + jx++] = el;
+        }
+        mat[ix++] = data + ix * n;
       }
+    }
+    ~Matrix() {
+      delete[] mat;
+      delete[] data;
     }
     const std::pair<std::size_t, std::size_t> size() const { return std::make_pair(m, n); }
     const std::size_t rows() const { return m; }
     const std::size_t cols() const { return n; }
+    const bool isSquare() { return m == n; }
     const T trace() const {
       T tr = 0;
-      for (std::size_t i = 0; i < mat.size(); i++) tr += mat[i][i];
+      for (std::size_t i = 0; i < std::min(m, n); i++) tr += mat[i][i];
       return tr;
     }
-    Vector<T>& operator[](const std::size_t i) { return mat[i]; }
-    const Vector<T>& operator[](const std::size_t i) const { return mat[i]; }
+    const T determinant() const {
+      checkSquare();
+      x3 = 0;
+      if (m >= GAUSS_ELIM_DET_THRESHOLD) return gaussDet(mat, m);
+      auto d = rdet(mat, m);
+      std::cout << "3x3: " << x3 << "\n";
+      return d;
+    }
+    T* operator[](const std::size_t i) { return mat[i]; }
+    const T* operator[](const std::size_t i) const { return mat[i]; }
     friend Matrix<T> operator*(const Matrix<T>& lhs, const Matrix<T>& rhs) {
       checkDimensions(lhs, rhs);
       auto lr = lhs.size().first;
@@ -204,11 +260,10 @@ namespace VecMat {
 
       std::cout << "lr " << lr << " lc " << lc << " rc " << rc << "\n";
       Matrix<T> ma(lr, rc);
-      std::cout << "++---+ " << ma[0].size() << "\n";
       for (auto i = 0; i < lr; i++) {
-        auto& lv = lhs[i];
+        auto lv = lhs[i];
         for (auto j = 0; j < rr; j++) {
-          auto& rv = rhs[j];
+          auto rv = rhs[j];
           for (auto k = 0; k < rc; k++) {
             ma[i][k] += lv[j] * rv[k];
           }
@@ -225,7 +280,7 @@ namespace VecMat {
 
       Vector<T> vec(mr);
       for (auto i = 0; i < mr; i++) {
-        auto& row = m[i];
+        auto row = m[i];
         for (auto j = 0; j < mc; j++) {
           vec[i] += row[j] * v[j];
         }
@@ -233,25 +288,33 @@ namespace VecMat {
       return vec;
     }
     friend std::ostream& operator<<(std::ostream& os, const Matrix<T>& m) {
-      if (m.mat.size()) {
+      if (m.rows()) {
         std::string lf = "";
-        for (auto& r : m.mat) {
-          os << lf << r;
+        const_iterator it = m.cbegin();
+        while (it != m.cend()) {
+          os << lf << "|";
+          std::string sep = "";
+          std::for_each(*it, *it + m.n, [&](const T& t) {
+            os << sep << t;
+            sep = " ";
+          });
+          os << "|";
           lf = '\n';
+          it++;
         }
       } else
         os << "||";
       return os;
     }
 
-    inline iterator begin() noexcept { return mat.begin(); }
-    inline const_iterator cbegin() const noexcept { return mat.cbegin(); }
-    inline iterator end() noexcept { return mat.end(); }
-    inline const_iterator cend() const noexcept { return mat.cend(); }
-    inline reverse_iterator rbegin() noexcept { return mat.rbegin(); }
-    inline reverse_iterator crbegin() const noexcept { return mat.crbegin(); }
-    inline reverse_iterator rend() noexcept { return mat.rend(); }
-    inline reverse_iterator crend() const noexcept { return mat.crend(); }
+    inline iterator begin() noexcept { return mat; }
+    inline const_iterator cbegin() const noexcept { return const_cast<const double**>(mat); }
+    inline iterator end() noexcept { return mat + m; }
+    inline const_iterator cend() const noexcept { return const_cast<const double**>(mat + m); }
+    inline reverse_iterator rbegin() noexcept { return mat + m; }
+    inline reverse_iterator crbegin() const noexcept { return const_cast<const double**>(mat + m); }
+    inline reverse_iterator rend() noexcept { return mat; }
+    inline reverse_iterator crend() const noexcept { return const_cast<const double**>(mat); }
 
    private:
     static void checkDimensions(const Matrix<T>& left, const Matrix<T>& right) {
@@ -264,10 +327,108 @@ namespace VecMat {
         throw(std::runtime_error("Matrix x vector length mismatch"));
       }
     }
+    static void checkEqualDimensions(const Matrix<T>& left, const Matrix<T>& right) {
+      if (left.size().first != right.size().first || left.size().second != right.size().sexond) {
+        throw(std::runtime_error("Matrix + matrix length mismatch"));
+      }
+    }
+    const void checkSquare() const {
+      if (m != n) {
+        throw(std::runtime_error("Matrix not square"));
+      }
+    }
+    const T rdet(T** const msub, const std::size_t sz) const {
+      T det(0);
+      if (sz < 1) {
+        throw(std::runtime_error("Matrix empty"));
+      }
+
+      if (sz == 1)
+        det = msub[0][0];
+      else if (sz == 2)
+        det = msub[0][0] * msub[1][1] - msub[1][0] * msub[0][1];
+      else if (sz == 3) {
+        x3++;
+        det = (msub[0][0] * (msub[1][1] * msub[2][2] - msub[2][1] * msub[1][2])) -
+              (msub[0][1] * (msub[1][0] * msub[2][2] - msub[1][2] * msub[2][0])) +
+              (msub[0][2] * (msub[1][0] * msub[2][1] - msub[1][1] * msub[2][0]));
+      } else {
+        auto mind = sz - 1;
+        T* d = new T[mind * mind];
+        T** t = new T*[mind];
+        for (auto i = 0; i < mind; i++) {
+          t[i] = d + i * mind;
+        }
+
+        int_least16_t p = 1;
+        for (auto i = 0; i < sz; i++) {
+          getMinor(msub, t, 0, i, sz);
+          det += rdet(t, sz - 1) * msub[0][i] * p;
+          p = -p;
+        }
+
+        delete[] t;
+        delete[] d;
+      }
+
+      return det;
+    }
+
+    const void getMinor(T** const m, T** const t, const std::size_t i, const std::size_t j, const std::size_t n) const {
+      int ix, jx, ic = 0, jc;
+
+      for (ix = 0; ix < n; ix++) {
+        if (i == ix) continue;
+        jc = 0;
+        for (jx = 0; jx < n; jx++) {
+          if (j == jx) continue;
+          t[ic][jc] = m[ix][jx];
+          jc++;
+        }
+        ic++;
+      }
+    }
+
+    const T gaussDet(T** const msub, const std::size_t sz) const {
+      T det(1);
+      T* ad = new T[sz * sz];
+      T** a = new T*[sz];
+      std::copy(data, data + sz * sz, ad);
+      for (auto i = 0; i < sz; i++) a[i] = ad + i * sz;
+
+      uint parity = 0;
+
+      for (auto i = 0; i < sz; i++) {
+        for (auto k = i + 1; k < sz; k++) {
+          if (std::abs(a[i][i]) < std::abs(a[k][i])) {
+            parity ^= 1;
+            for (auto j = 0; j < sz; j++) {
+              std::swap(a[i][j], a[k][j]);
+            }
+          }
+        }
+      }
+
+      for (auto i = 0; i < sz - 1; i++) {
+        for (auto k = i + 1; k < sz; k++) {
+          T t = a[k][i] / a[i][i];
+          for (auto j = 0; j < sz; j++) {
+            a[k][j] = a[k][j] - t * a[i][j];
+          }
+        }
+      }
+
+      for (auto i = 0; i < sz; i++) det *= a[i][i];
+
+      return parity ? -det : det;
+    }
+
     std::size_t m;
     std::size_t n;
-    std::vector<Vector<T>> mat;
-  };
+    T* data;
+    T** mat;
+    mutable uint x3;
+  };  // namespace VecMat
 
   template <typename T>
   const inline Matrix<T> transpose(const Matrix<T>& m) {
@@ -279,6 +440,30 @@ namespace VecMat {
     }
 
     return t;
+  }
+
+  template <typename T>
+  const inline Matrix<T> conj(const Matrix<T>& m) {
+    Matrix<T> ma(m.size());
+    for (auto i = 0; i < m.size().second; i++) {
+      for (auto j = 0; j < m.size().first; j++) {
+        ma[i][j] = std::conj(m[i][j]);
+      }
+    }
+
+    return ma;
+  }
+
+  template <typename T>
+  const inline Matrix<T> hermitian(const Matrix<T>& m) {
+    Matrix<T> ma(m.size().second, m.size().first);
+    for (auto i = 0; i < m.size().second; i++) {
+      for (auto j = 0; j < m.size().first; j++) {
+        ma[j][i] = std::conj(m[i][j]);
+      }
+    }
+
+    return ma;
   }
 }  // namespace VecMat
 #endif
